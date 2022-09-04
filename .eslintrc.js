effa51c58363824ca8de222a966f159c850418ef
Iván García <ivan.garcia.external@thirdbridge.com>
2022-09-04 08:01:19 +0200

    chore: reorganize structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,11 +10,8 @@ module.exports = {
             semi: "off",
 
             // TYPESCRIPT
-            "@typescript-eslint/consistent-type-assertions": "off",
             "@typescript-eslint/explicit-function-return-type": "off",
             "@typescript-eslint/indent": ["error", 6],
-            "@typescript-eslint/no-namespace": "off",
-            "@typescript-eslint/promise-function-async": "off",
             "@typescript-eslint/semi": ["error", "always"],
             "@typescript-eslint/strict-boolean-expressions": "off"
       },
